2431ac410df5a5e4298e50feae3cd36f0e01d01d
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2023-05-29 23:21:38 -0300

    Adicionando coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,15 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+
   test: {
-    include: ['src/hooks/**'],
+    passWithNoTests: true,
     environment: 'jsdom',
-    exclude:  ['src/components/**', 'src/assets/**', 'src/layouts/**', 'src/styles/**', 'src/pages/**', 'src/contexts/**', 'src/api/**'],
+    exclude:  ['node_modules/**', 'dist/**','src/components/**', 'src/assets/**', 'src/layouts/**', 'src/styles/**', 'src/pages/**', 'src/contexts/**', 'src/api/**'],
     coverage: {
-        exclude: ['src/components/**', 'src/assets/**', 'src/layouts/**', 'src/styles/**', 'src/pages/**', 'src/contexts/**', 'src/api/**']
+        reporter: ['lcov', "html"],
+        provider: 'c8',
+        exclude: ['node_modules/**', 'dist/**','src/components/**', 'src/assets/**', 'src/layouts/**', 'src/styles/**', 'src/pages/**', 'src/contexts/**', 'src/api/**']
     }
   },
 })
